20bacad253a9bbab6bdddad0bffe55d010d38ecd
HIMANSHU ANAND <himanshuanand563@gmail.com>
2025-09-04 19:11:46 +0000

    make setup

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,9 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {config} from 'dotenv';
+
+config();
 
 export const ai = genkit({
   plugins: [googleAI()],
